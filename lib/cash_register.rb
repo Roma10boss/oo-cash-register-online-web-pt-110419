8077ec12197dd078609073b667f318c7c464eeb8
Romario Gustave <romagustave10@gmail.com>
2019-12-31 18:31:14 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,4 +7,27 @@ class CashRegister
     items = []
   end 
   
+  def add_item(items, price, quantity = 1)
+    @price = price 
+    @total += price + quantity
+    if quantity = 1 
+      counter = 0 
+    end 
+  else @items = items
+  end 
+end 
+
+  def apply_discount 
+    if @discount > 0 
+      @to_take_off = (price + discount)/ 100 
+      @total -= @to_take_off 
+      return "Your total after discount is: #{total}."
+    else 
+      return "there's no discount applied."
+    end 
+    def void_last_transaction
+      @total -= price 
+    end 
+    
+  
 end 
